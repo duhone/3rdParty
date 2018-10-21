42d6fff891f0f10714897d59e14910ab02126bfc
duhone <eric.duhon@oculus.com>
2018-10-20 19:05:56 -0500

    add rapidjson

--- build/build_glm.cmake
+++ build/build_glm.cmake
@@ -1,1 +1,19 @@
 include_directories("${CMAKE_CURRENT_LIST_DIR}/../glm")
+
+set(CMAKE_CXX_STANDARD 20)
+add_definitions(-DGLM_FORCE_CXX2A)
+
+if((CMAKE_CXX_COMPILER_ID MATCHES "Clang") OR (CMAKE_CXX_COMPILER_ID MATCHES "GNU"))
+	add_compile_options(-ffast-math)
+
+elseif(CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
+	add_compile_options(/fp:fast)
+endif()
+
+if((CMAKE_CXX_COMPILER_ID MATCHES "GNU") OR (CMAKE_CXX_COMPILER_ID MATCHES "Clang"))
+	add_compile_options(-mavx)
+elseif(CMAKE_CXX_COMPILER_ID MATCHES "Intel")
+	add_compile_options(/QxAVX)
+elseif(CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
+	add_compile_options(/arch:AVX)
+endif()
---
+++ build/build_rapidjson.cmake
@@ -0,0 +1,3 @@
+include_directories("${CMAKE_CURRENT_LIST_DIR}/../rapidjson/include")
+
+add_definitions(-DRAPIDJSON_HAS_STDSTRING)
